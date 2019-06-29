6654bf42fcd1026b18276e33e666b4888c13cdf1
Tobias <tobias@appysphere.com>
2019-06-29 13:52:41 +0700

    gcd with command line reading

--- programming_rust/target/debug/programming_rust-3dbfccbdcffe7fe4.d
+++
@@ -1,1 +0,0 @@
-/Users/tobias/Code/rust/programming_rust/target/debug/programming_rust-3dbfccbdcffe7fe4.dSYM: /Users/tobias/Code/rust/programming_rust/src/main.rs
--- programming_rust/target/debug/programming_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/tobias/Code/rust/programming_rust/target/debug/programming_rust.dSYM: /Users/tobias/Code/rust/programming_rust/src/main.rs
