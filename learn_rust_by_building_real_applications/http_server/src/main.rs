63b29a544db014be78ab5fadebd957de71518880
Tobias Leinss <caruso33@web.de>
2022-08-26 22:35:47 +0200

    Refactoring into modules

--- learn_rust_by_building_real_applications/http_server/src/main.rs
+++ learn_rust_by_building_real_applications/http_server/src/main.rs
@@ -1,18 +1,48 @@
+use http::request::Request;
+use server::Server;
+
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self { addr }
+mod server {
+    pub struct Server {
+        addr: String,
     }
 
-    fn run(self) -> () {
-        println!("Running server on {}", &self.addr);
+    impl Server {
+        pub fn new(addr: String) -> Self {
+            Self { addr }
+        }
+
+        pub fn run(self) -> () {
+            println!("Running server on {}", &self.addr);
+        }
+    }
+}
+mod http {
+    pub mod request {
+        pub enum Method {
+            GET,
+            POST,
+            PUT,
+            PATCH,
+            DELETE,
+            OPTIONS,
+            HEAD,
+            CONNECT,
+            TRACE,
+        }
+    }
+
+    pub mod method {
+        use super::request;
+
+        pub struct Request {
+            path: String,
+            query_string: Option<String>,
+            method: request::Method,
+        }
     }
 }
