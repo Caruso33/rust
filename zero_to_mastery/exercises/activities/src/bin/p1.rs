320fb64710dac9a9bca774a5aaa786961130acf8
Tobias Leinss <caruso33@web.de>
2022-10-02 22:00:37 +0200

    Work on view and add

--- zero_to_mastery/exercises/activities/src/bin/p1.rs
+++ zero_to_mastery/exercises/activities/src/bin/p1.rs
@@ -51,42 +51,47 @@ impl Bill {
 }
 
 fn main() {
-    let bill = Bill::new("first bill".to_string(), 123.32);
-
     let mut bills: Vec<Bill> = Vec::new();
-    bills.push(bill);
 
     let mut command: io::Result<String>;
     loop {
-        command = read_user_input();
+        print!("\x1B[2J\x1B[1;1H");
+        println!(
+            "Welcome to the Ultimate Bill Storage System.\n
+You have following Options:\n
+    view -> View current Bills\n
+    add -> Add a new Bill to the system\n
+    edit -> Edit an existing Bill in the system\n
+    remove -> Delete a Bill from the system\n
+Have fun!\n"
+        );
+        command = read_user_input(None, None);
 
         let action: Option<Command> = match command {
             Err(_) => continue,
             Ok(c) => evaluate_user_input(c.as_str()),
         };
 
-        // if let Err(_) = command {
-        //     continue;
-        // }
-
-        // let action = evaluate_user_input(&command.unwrap());
-
         match action {
             None => continue,
             Some(x) => perform_action(x, &mut bills),
         }
-        // if action.is_none() {
-        //     continue;
-        // }
-
-        // perform_action(action.unwrap(), &mut bills);
     }
 }
 
-fn read_user_input() -> io::Result<String> {
-    let mut buffer = String::new();
+fn read_user_input(
+    user_comment: Option<&str>,
+    pre_used_buffer: Option<String>,
+) -> io::Result<String> {
+    let mut buffer = match pre_used_buffer {
+        None => String::new(),
+        Some(x) => x,
+    };
 
-    println!("Enter command:");
+    match user_comment {
+        None => println!("Enter command:"),
+        Some(text) => println!("{}", text),
+    }
 
     io::stdin().read_line(&mut buffer)?;
 
@@ -106,14 +111,96 @@ fn evaluate_user_input(input: &str) -> Option<Command> {
 fn perform_action(command: Command, bills: &mut Vec<Bill>) {
     match command {
         Command::View => view(bills),
-        Command::Add => println!("Not implemented yet"),
+        Command::Add => add(bills),
         Command::Remove => println!("Not implemented yet"),
         Command::Edit => println!("Not implemented yet"),
     }
 }
 
 fn view(bills: &mut Vec<Bill>) {
+    println!("\nCurrent Bills:\n");
     for bill in bills {
         println!("{:?}", bill);
+    }
+    read_user_input(Some(""), None);
+}
+
+fn add(bills: &mut Vec<Bill>) {
+    let mut name: io::Result<String>;
+    let mut amount: io::Result<String>;
+    let mut confirmation: io::Result<String>;
+
+    loop {
+        let pre_used_name = None;
+        // let pre_used_name = if name.is_ok() && name.unwrap().len() > 0 {
+        //     Some(name.unwrap());
+        // } else {
+        //     None;
+        // };
+
+        name = read_user_input(Some("Enter Bill Name"), pre_used_name);
+
+        match name {
+            Err(_) => continue,
+            Ok(_) => break,
+        }
+    }
+
+    loop {
+        let pre_used_amount = None;
+        // let pre_used_amount = if amount.is_ok() && amount.unwrap().len() > 0 {
+        //     Some(amount.unwrap());
+        // } else {
+        //     None;
+        // };
+        amount = read_user_input(Some("Enter Bill Amount"), pre_used_amount);
+
+        if amount.is_err() {
+            continue;
+        }
+
+        match &amount {
+            Err(_) => continue,
+            Ok(a) => {
+                let float = a.parse::<f32>();
+                match float {
+                    Ok(_) => break,
+                    Err(_) => continue,
+                }
+            }
+        }
+    }
+
+    loop {
+        confirmation = read_user_input(
+            Some(
+                format!(
+                    "Name: {}, Amount: {}, is this correct? ([y]es/[n]o/[a]bort)",
+                    name.as_ref().unwrap(),
+                    amount.as_ref().unwrap()
+                )
+                .as_str(),
+            ),
+            None,
+        );
+
+        match confirmation {
+            Err(_) => continue,
+            Ok(c) => {
+                if c.to_lowercase() == "y" {
+                    let bill = Bill::new(name.unwrap(), amount.unwrap().parse::<f32>().unwrap());
+                    bills.push(bill);
+                    read_user_input(Some("Bill added..."), None);
+                    return;
+                } else if c.to_lowercase() == "n" {
+                    add(bills);
+                    return;
+                } else if c.to_lowercase() == "a" {
+                    return;
+                } else {
+                    continue;
+                }
+            }
+        }
     }
 }
